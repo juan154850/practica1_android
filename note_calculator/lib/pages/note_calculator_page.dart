14016d2bd419e3dd025a4de28e61caceef930199
Juan Bermudez <juan052115@gmail.com>
2024-09-16 13:27:13 -0600

    agregar el permiso de internet

--- note_calculator/lib/pages/note_calculator_page.dart
+++ note_calculator/lib/pages/note_calculator_page.dart
@@ -8,6 +8,38 @@ class NoteCalculatorPage extends StatefulWidget {
 }
 
 class _NoteCalculatorPageState extends State<NoteCalculatorPage> {
+  bool isCalculating = false;
+  double laboratory = 0.0;
+  double firstProject = 0.0;
+  double secondProject = 0.0;
+  double finalProject = 0.0;
+
+  double calculateFinalNote(double laboratory, double firstProject,
+      double secondProject, double finalProject) {
+    setState(() {
+      isCalculating = true;
+    });
+
+    double result;
+    if ((laboratory != 0) &&
+        (firstProject != 0) &&
+        (secondProject != 0) &&
+        (finalProject != 0)) {
+      result = (laboratory * 0.6) +
+          (firstProject * 0.1) +
+          (secondProject * 0.08) +
+          (finalProject * 0.25);
+    } else {
+      result = 0.0;
+    }
+
+    setState(() {
+      isCalculating = false;
+    });
+
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +50,19 @@ class _NoteCalculatorPageState extends State<NoteCalculatorPage> {
       body: const Padding(
         padding: EdgeInsets.all(20.0),
         child: Center(
-          child: Text("Contenido de la app"),
+          child: Column(
+            children: [
+              SizedBox(height: 20.0),
+              //input de la primera nota
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Nota de laboratorio',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+            ],
+          ),
         ),
       ),
     );
