dfc355b800e9135c4d67e13354ba179366869652
Juan Bermudez <juan052115@gmail.com>
2024-09-21 11:54:02 -0600

    crear la app para calcula la nota final, implementando validaciones de los datos ingresados y limpieza de los mismos.

--- note_calculator/lib/main.dart
+++ note_calculator/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.teal,
         useMaterial3: true,
       ),
-      home: const MyWidgetPage(),
+      home: const NoteCalculatorPage(),
     );
   }
 }
--- note_calculator/lib/pages/note_calculator_page.dart
+++ note_calculator/lib/pages/note_calculator_page.dart
@@ -1,31 +1,99 @@
 import 'package:flutter/material.dart';
 
-class MyWidgetPage extends StatefulWidget {
-  const MyWidgetPage({super.key});
+class NoteCalculatorPage extends StatefulWidget {
+  const NoteCalculatorPage({super.key});
 
   @override
-  State<MyWidgetPage> createState() => _MyWidgetPageState();
+  State<NoteCalculatorPage> createState() => _NoteCalculatorPageState();
 }
 
-class _MyWidgetPageState extends State<MyWidgetPage> {
+class _NoteCalculatorPageState extends State<NoteCalculatorPage> {
   final _laboratory = TextEditingController();
   final _firstNote = TextEditingController();
+  final _secondNote = TextEditingController();
+  final _finalProjectNote = TextEditingController();
   double _finalNote = 0;
 
-  void _calculateFinalNote() {
+  bool _error = false;
+  String _errorMessage = '';
+
+  bool _calculateNotePressed = false;
+
+  bool _validateNotes() {
     if (_laboratory.text.isEmpty ||
-        double.parse(_laboratory.text) < 0 ||
-        double.parse(_laboratory.text) > 5) {
-      _finalNote = 0;
-      return;
+        double.parse(_laboratory.text.replaceAll(',', '.')) < 0 ||
+        double.parse(_laboratory.text.replaceAll(',', '.')) > 5) {
+      setState(() {
+        _error = true;
+        _errorMessage =
+            'Por favor verifique el valor ingresado para la nota de laboratorio';
+      });
+      return false;
     }
-    final laboratory = double.parse(_laboratory.text);
-    final firstNote = double.parse(_firstNote.text);
-    final finalNote = ((laboratory * 0.6)+(firstNote * 0.1));
-    //dejar con 2 decimales
+    if (_firstNote.text.isEmpty ||
+        double.parse(_firstNote.text.replaceAll(',', '.')) < 0 ||
+        double.parse(_firstNote.text.replaceAll(',', '.')) > 5) {
+      setState(() {
+        _error = true;
+        _errorMessage =
+            'Por favor verifique el valor ingresado para la primera entrega del proyecto final';
+      });
+      return false;
+    }
+    if (_secondNote.text.isEmpty ||
+        double.parse(_secondNote.text.replaceAll(',', '.')) < 0 ||
+        double.parse(_secondNote.text.replaceAll(',', '.')) > 5) {
+      setState(() {
+        _error = true;
+        _errorMessage =
+            'Por favor verifique el valor ingresado para la segunda entrega del proyecto final';
+      });
+      return false;
+    }
+    if (_finalProjectNote.text.isEmpty ||
+        double.parse(_finalProjectNote.text.replaceAll(',', '.')) < 0 ||
+        double.parse(_finalProjectNote.text.replaceAll(',', '.')) > 5) {
+      setState(() {
+        _error = true;
+        _errorMessage =
+            'Por favor verifique el valor ingresado para la entrega del proyecto final';
+      });
+      return false;
+    }
     setState(() {
+      _error = false;
+      _errorMessage = '';
+    });
+    return true;
+  }
+
+  void _calculateFinalNote() {
+    setState(() {
+      _calculateNotePressed = true;
+      if (!_validateNotes()) {
+        _finalNote = 0;
+        return;
+      }
+      final laboratory = double.parse(_laboratory.text.replaceAll(',', '.'));
+      final firstNote = double.parse(_firstNote.text.replaceAll(',', '.'));
+      final secondNote = double.parse(_secondNote.text.replaceAll(',', '.'));
+      final finalProjectNote =
+          double.parse(_finalProjectNote.text.replaceAll(',', '.'));
+      final finalNote = ((laboratory * 0.6) +
+          (firstNote * 0.1) +
+          (secondNote * 0.05) +
+          (finalProjectNote * 0.25));
       _finalNote = (double.parse(finalNote.toStringAsFixed(2)));
     });
+  }
+
+  @override
+  void dispose() {
+    _laboratory.dispose();
+    _firstNote.dispose();
+    _secondNote.dispose();
+    _finalProjectNote.dispose();
+    super.dispose();
   }
 
   @override
@@ -38,57 +106,77 @@ class _MyWidgetPageState extends State<MyWidgetPage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Laboratorio (60%)',
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Image(
+                  image: AssetImage('assets/images/logo.png'),
+                  width: 80,
+                  height: 80,
                 ),
-                keyboardType: TextInputType.number,
-                controller: _laboratory,
-                //minimo 0 maximo 5
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese un valor';
-                  }
-                  if (double.parse(value) < 0 || double.parse(value) > 5) {
-                    return 'La nota debe ser un valor entre 0 y 5';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Primera entrega proyecto final (10%)',
+                const SizedBox(height: 26),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Laboratorio (60%)',
+                    prefixIcon: Icon(Icons.science),
+                    helperText: '* Nota entre 0 y 5 ',
+                  ),
+                  keyboardType: TextInputType.number,
+                  controller: _laboratory,
                 ),
-                keyboardType: TextInputType.number,
-                controller: _firstNote,
-                //minimo 0 maximo 5
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese un valor';
-                  }
-                  if (double.parse(value) < 0 || double.parse(value) > 5) {
-                    return 'La nota debe ser un valor entre 0 y 5';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: _calculateFinalNote,
-                child: const Text('Calcular nota final'),
-              ),
-              const SizedBox(height: 16),
-              Text(
-                'Nota final: $_finalNote',
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Primera entrega proyecto final (10%)',
+                    helperText: '* Nota entre 0 y 5 ',
+                    prefixIcon: Icon(Icons.assignment),
+                  ),
+                  keyboardType: TextInputType.number,
+                  controller: _firstNote,
                 ),
-              ),
-            ],
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Segunda entrega proyecto final (5%)',
+                    helperText: '* Nota entre 0 y 5 ',
+                    prefixIcon: Icon(Icons.assignment),
+                  ),
+                  keyboardType: TextInputType.number,
+                  controller: _secondNote,
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Entrega proyecto final (25%)',
+                    helperText: '* Nota entre 0 y 5 ',
+                    prefixIcon: Icon(Icons.assignment),
+                  ),
+                  keyboardType: TextInputType.number,
+                  controller: _finalProjectNote,
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _calculateFinalNote,
+                  child: const Text('Calcular nota final'),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  _error && _calculateNotePressed
+                      ? _errorMessage
+                      : 'Nota final: $_finalNote',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: _error ? 14 : 24,
+                    color: _error
+                        ? Colors.red
+                        : _finalNote >= 3
+                            ? Colors.green
+                            : Colors.red,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
